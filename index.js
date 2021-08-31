b557bdc2829ce9b9c2c19595e4082a387bc771f8
Jeffery Patterson <jpatterson933@ucla.edu>
2021-08-30 22:02:42 -0700

    replaced old api key with new secret api key

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-fetch("https://alpha-vantage.p.rapidapi.com/query?function=OVERVIEW&symbol=MSFT&datatype=json&output_size=compact", {
+
+
+fetch("https://alpha-vantage.p.rapidapi.com/query?interval=5min&function=TIME_SERIES_INTRADAY&symbol=MSFT&datatype=json&output_size=compact", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
-		"x-rapidapi-key": "keyhidden"
+		"x-rapidapi-key": config
 	}
 })
 .then(response => {
 	console.log(response);
-    return response.json()
+	return response.json();
 })
 .then(data => {
-    console.log(data)
+	console.log(data)
 })
 .catch(err => {
 	console.error(err);
