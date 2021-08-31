0b69b49caea5bdb389dbb441496821fdf9361c41
Jeffery Patterson <jpatterson933@ucla.edu>
2021-08-30 22:43:49 -0700

    Added test.js for testing purposes. Also added drop down for two ticker symbols that will be fetched when the user clicks show. Also broke down our fetch so it now lies in a function

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 
 
-fetch("https://alpha-vantage.p.rapidapi.com/query?interval=5min&function=TIME_SERIES_INTRADAY&symbol=MSFT&datatype=json&output_size=compact", {
+fetch("https://alpha-vantage.p.rapidapi.com/query?function=OVERVIEW&symbol=MSFT&datatype=json&output_size=compact", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
 		"x-rapidapi-key": config
 	}
 })
-.then(response => {
-	console.log(response);
-	return response.json();
+	.then(response => {
+		console.log(response);
+		return response.json();
+	})
+	.then(data => {
+		console.log(data)
+	})
+	.catch(err => {
+		console.error(err);
+	});
+
+// our fetch query function
+const getCompanyDescription = (dropdownSelection) => {
+	const baseUrl = "https://alpha-vantage.p.rapidapi.com/query?function=OVERVIEW&symbol=";
+	const query = baseUrl + dropdownSelection + "&datatype=json&output_size=compact";
+	console.log(query)
+	return fetch(query, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
+			"x-rapidapi-key": config
+		}
+	})
+}
+
+$("#ticker-choice").on("click", (e) => {
+	e.preventDefault();
+	const tickerSymbol = $("#ticker").val();
+	getCompanyDescription(tickerSymbol)
+	.then(response => {
+		console.log(response)
+		return response.json();
+	})
+	.then(data => {
+		console.log(data)
+	})
+	.catch(err => {
+		console.error(err)
+	})
 })
-.then(data => {
-	console.log(data)
-})
-.catch(err => {
-	console.error(err);
-});
---
+++ test.js
@@ -0,0 +1,5 @@
+// A javascript file to test elements before bringing them to the main index.js
+
+const ticker = $("#ticker").val();
+
+console.log(ticker)
